d42aeedd8691d7cdf7327d28f15ff4556a91ded5
Wren <wrenparker@gmail.com>
2022-01-17 15:37:21 -0500

    changed the bookmark hover color, reused the event.php popup again

--- views/User.php
+++ views/User.php
@@ -10,19 +10,8 @@
       </div>
       <hr>
       <div class="row my-4">
-        <?php foreach ($bookmarks as $bookmark): ?>
-          <div class="text-center">
-            <a href="routes.php?action=viewEvent&id=<?php echo $bookmark->id ?>">
-              <div class="mt-1 mx-1 mb-0 p-2 bookmark bookmarked-event">
-                <div class="row mx-2 justify-content-between">
-                  <div class="col px-2">
-                    <?php echo $bookmark->name ?>
-                  </div>
-                </div>
-
-              </div>
-            </a>
-          </div>
+        <?php foreach ($bookmarks as $event): ?>
+          <?php include 'include/schedule/event.php'; ?>
         <?php endforeach; ?>
       </div>
       <?php include 'include/schedule/full-schedule.php' ?>
